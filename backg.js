f6e6b7e4a30e30b42bdfc2a2aa1dd428f316da23
manvirclair <manvirclair@gmail.com>
2019-02-11 12:23:18 +0530

    checkboxed pdf links select all or some

--- backg.js
+++ backg.js
@@ -1,4 +1,5 @@
 chrome.runtime.onInstalled.addListener(function() {
+  
     chrome.storage.sync.set({color: '#3aa757'}, function() {
       console.log("The color is green.");
     });
@@ -13,3 +14,37 @@ chrome.runtime.onInstalled.addListener(function() {
     });
   });
 
+
+chrome.runtime.onStartup.addListener(function () {
+  console.log("ext startup")
+})
+
+  chrome.contextMenus.create({
+    id: "aid",
+    title: "Test Download %s", 
+    contexts:["selection"], 
+  });
+
+function saveSelection(info, tab)
+{
+  var imgurl = info.selectionText;
+  console.log("downloading " + info.selectionText)
+  chrome.downloads.download({url:imgurl},function(downloadId){
+      console.log("download begin, the downId is:" + downloadId);
+  });
+}
+
+chrome.contextMenus.onClicked.addListener(saveSelection);
+chrome.extension.onMessage.addListener(dllinks)
+
+function dllinks(links) {
+  for (var link in links) {
+    if (links[link]) {
+      chrome.downloads.download(
+        {
+          url: links[link],
+        }
+      )
+    }
+  }
+}
--- linklist.js
+++ linklist.js
@@ -1,11 +1,62 @@
 console.log("linklist ran");
 var array = [];
-var listhtml = ''
+var tree = null;
+var listhtml = '<div id = "pdflinks" class="tree"><button type="button" id="dllinks" onclick="dllinks"> "download"</button>\
+<input type="textbox" id="dlocation"/>\
+<input type="checkbox" /> All<div class="lnks"><ol>'
 var links = document.links;
+    // array.push(links[0].href);
+    // listhtml += ('<input type="checkbox" />' + links[0].href )
+    
 for(var i=0; i<links.length; i++) {
-  array.push(links[i].href);
-  listhtml += ('<li>' +  links[i].href +  '</li>')
+  if (links[i].getAttribute('href').endsWith('.pdf')) {
+    // array.push(links[i].href);
+    listhtml += ('<li><input type="checkbox" /><a href="' + links[i].href + '">' +  links[i].href + '</a></li>' ) 
+  }
 }
-var z = document.createElement('ol');
+listhtml += '</ol></div></div>'
+var z = document.createElement('div');
+
 z.innerHTML = listhtml;
 document.body.appendChild(z);
+
+function dllinks() {
+chrome.extension.sendMessage(array);
+}
+
+document.getElementById('dllinks').addEventListener("click", dllinks)
+$(document).ready(function(){
+  $('.tree input[type="checkbox"]').on('change', function() {
+    checkChild($(this));
+  });
+
+  function checkChild(element) {
+    // console.log($('dlocation').get().value)
+    if (element.parent().prop('class') != 'lnks') {
+      var children = element.next().children();
+      if (element.prop('checked')) {
+
+        for (var i in children) {
+          children[i].checked = true;
+          array.push(children[i].nextElementSibling.href);
+        }
+
+      }
+      else {
+        for (var i in children) {
+          children[i].checked = false;
+          array.pop(children[i].nextElementSibling.href);
+        }
+      }
+    }
+    else{
+      if (element.prop('checked')){
+        array.push(element.next().prop('href'));
+      }
+      else{
+        array.pop(element.next().prop('href'));
+
+      }
+    }
+  }
+})
